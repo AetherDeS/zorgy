88468ffa51ed18b11e6d2a6597c965f3fc14d304
AetherDeS <12rambler12@gmail.com>
2025-12-09 17:55:51 +0300

    editing main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import click
 
+
+@click.command()
+@click.option('--directories', prompt="Choose directories", help='Available directories /home, /etc, /var, /srv, /opt')
+def main():
+    click.echo("Some description of this util")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
